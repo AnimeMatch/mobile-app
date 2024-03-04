36490b79665355e6653c1ba63f7083f98b586145
AlexanderKimura <alexander.silva@sptech.school>
2024-03-03 23:44:53 -0300

    inicia projeto pf

---
+++ animematchmobileapp/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "animematch-mobile-app"
+include(":app")
+ 

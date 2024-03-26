48672af3e63cf3b8838853c2dd28ac2d5dbd6b18
AlexanderKimura <alexander.silva@sptech.school>
2024-03-26 01:06:03 -0300

    status bar component

---
+++ AnimeMatch/app/src/main/java/com/myApp/ui/screens/PerfilActivity.kt
@@ -0,0 +1,57 @@
+package com.myApp.ui.screens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.myApp.ui.components.Carrossel
+import com.myApp.ui.components.StatusBar
+import com.myApp.ui.theme.AnimeMatchTheme
+import com.myApp.ui.theme.blueBgAM
+
+class PerfilActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimeMatchTheme {
+                Perfil()
+            }
+        }
+    }
+}
+
+@Composable
+fun Perfil() {
+    Box(
+        modifier = Modifier
+            .background(blueBgAM)
+            .fillMaxSize()
+            .verticalScroll(ScrollState(0))
+    ){
+        Column {
+            StatusBar()
+            Spacer(modifier = Modifier.padding(vertical = 40.dp))
+            Carrossel("Animes favoritos")
+            Spacer(modifier = Modifier.padding(vertical = 16.dp))
+            Carrossel("Mangás favoritos")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PerfilActivityPreview() {
+    Perfil()
+}

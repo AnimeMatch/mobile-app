1b2cc5e51e5fca02e14879563d90f1aac6db36fb
Sofia Bastos <sofia.bastos@sptech.school>
2024-03-26 19:25:48 -0300

    ajustes
    ajustes na tela de cadastro

--- AnimeMatch/app/src/main/java/com/myApp/ui/screens/RegisterScreen.kt
+++ AnimeMatch/app/src/main/java/com/myApp/ui/screens/RegisterScreen.kt
@@ -1,12 +1,10 @@
 package com.myApp.ui.screens
 
 import android.os.Bundle
-import android.widget.Switch
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,12 +15,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -32,16 +37,31 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.animematch.R
 import com.myApp.ui.theme.AnimeMatchTheme
+import com.myApp.ui.theme.Poppins
+import com.myApp.ui.theme.Roboto
+import com.myApp.ui.theme.blueAM
+import com.myApp.ui.theme.blueBgAM
+import com.myApp.ui.theme.blueBgAMTranparent
 import com.myApp.ui.theme.yellowAM
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class RegisterScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,50 +77,50 @@ class RegisterScreenActivity : ComponentActivity() {
 @Composable
 fun RegisterScreen() {
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                brush = androidx.compose.ui.graphics.Brush.verticalGradient(
-                    colors = listOf(
-                        Color(0xFF000B1C),
-                        Color(0xFF000B1C),
-                        Color(0xFF000B50),
-                        Color(0xFF000B50),
-                        Color(0xFF000770)
+        modifier = with(Modifier) {
+            fillMaxSize()
+                .paint(
+                    painterResource(id = R.drawable.background),
+                    contentScale = ContentScale.FillBounds,
+                )
+                .background(
+                    brush = androidx.compose.ui.graphics.Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0xFF000712).copy(1f),
+                            Color(0xFF000712).copy(.8f),
+                            Color(0xFF000712).copy(0.3f)
+                        )
                     )
                 )
-            )
+        }
     ) {
 
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(25.dp)
-
+                .padding(25.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
             Row(
                 modifier = Modifier
-                    .fillMaxWidth()
-
-
+                    .widthIn(60.dp, 620.dp)
+                    .padding(bottom = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(
                     text = "Cadastro",
-                    fontWeight = FontWeight.Bold,
+                    fontFamily = Poppins,
+                    fontWeight = FontWeight(700),
                     color = Color.White,
                     fontSize = 45.sp,
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier
-                        .padding(
-                            vertical = 55.dp
-                        )
                 )
+                Spacer(modifier = Modifier.width(25.dp))
                 Divider(
                     modifier = Modifier
-                        .padding(vertical = 90.dp)
-                        .padding(horizontal = 15.dp)
-                        .height(3.dp),
+                        .height(3.dp)
+                        .fillMaxWidth(),
                     color = yellowAM,
                 )
             }
@@ -112,33 +132,50 @@ fun RegisterScreen() {
                 Text(
                     text = "Nome de usuário",
                     color = Color.White,
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier
-                        .fillMaxWidth()
-
+                    fontFamily = Poppins,
+                    fontWeight = FontWeight(300)
                 )
-                var email by remember {
+
+                var usuario by remember {
                     mutableStateOf("")
                 }
 
                 OutlinedTextField(
+                    value = usuario,
+                    onValueChange = { newText ->
+                        usuario = newText
+                    },
+                    placeholder = {
+                        Text(
+                            text = "batatinha123",
+                            fontFamily = Roboto,
+                            color = blueBgAMTranparent,
+                            fontSize = 12.sp
+                        )
+                    },
+                    textStyle = TextStyle(
+                        fontFamily = Roboto,
+                        fontSize = 12.sp,
+                        color = blueBgAM
+                    ),
+                    singleLine = true,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = blueAM,
+                        unfocusedBorderColor = Color.White,
+                        errorBorderColor = Color.Red,
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color.White,
+                        cursorColor = blueBgAM
+                    ),
+                    shape = RoundedCornerShape(8.dp),
                     modifier = Modifier
-                        .height(30.dp)
                         .fillMaxWidth()
-                        .clip(MaterialTheme.shapes.small)
-                        .background(color = Color.White),
+                )
 
-                    value = email,
-                    onValueChange = { email = it },
-
-                    )
             }
 
             /*COLUNA PARA SEPARAR NOME E EMAIL*/
-            Column(
-                modifier = Modifier.padding(6.dp)
-            ) {
-            }
+            Spacer(modifier = Modifier.height(24.dp))
 
 //*COLUNA DO EMAIL*
             Column(
@@ -149,61 +186,132 @@ fun RegisterScreen() {
                 Text(
                     text = "E-mail",
                     color = Color.White,
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier
-
+                    fontFamily = Poppins,
+                    fontWeight = FontWeight(300)
                 )
 
-                var senha by remember {
+                var email by remember {
                     mutableStateOf("")
                 }
 
                 OutlinedTextField(
+                    value = email,
+                    onValueChange = { newText ->
+                        email = newText
+                    },
+                    placeholder = {
+                        Text(
+                            text = "example@gmail.com",
+                            fontFamily = Roboto,
+                            color = blueBgAMTranparent,
+                            fontSize = 12.sp
+                        )
+                    },
+                    textStyle = TextStyle(
+                        fontFamily = Roboto,
+                        fontSize = 12.sp,
+                        color = blueBgAM
+                    ),
+                    singleLine = true,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = blueAM,
+                        unfocusedBorderColor = Color.White,
+                        errorBorderColor = Color.Red,
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color.White,
+                        cursorColor = blueBgAM
+                    ),
+                    shape = RoundedCornerShape(8.dp),
                     modifier = Modifier
-                        .height(30.dp)
                         .fillMaxWidth()
-                        .clip(shape = MaterialTheme.shapes.small)
-                        .background(color = Color.White),
-                    value = senha,
-                    onValueChange = { senha = it }
                 )
             }
-            Column(
-                modifier = Modifier.padding(6.dp)
-            ) {
-            }
+
+            Spacer(modifier = Modifier.height(24.dp))
 
             /*COLUNA DA SENHA*/
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .widthIn(60.dp, 620.dp)
 
             ) {
-                Text(
-                    text = "Senha",
-                    color = Color.White,
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier
-                )
-
                 var senha by remember {
                     mutableStateOf("")
                 }
 
-                OutlinedTextField(
-                    modifier = Modifier
-                        .height(30.dp)
-                        .fillMaxWidth()
-                        .clip(shape = MaterialTheme.shapes.small)
-                        .background(color = Color.White),
-                    value = senha,
-                    onValueChange = { senha = it }
+                var passwordVisible by remember {
+                    mutableStateOf(false)
+                }
+
+                var hidePasswordJob by remember { mutableStateOf<Job?>(null) }
+
+                fun startHidePasswordTimer() {
+                    hidePasswordJob?.cancel()
+                    hidePasswordJob = CoroutineScope(Dispatchers.Default).launch {
+                        delay(1000) // Tempo em milissegundos para ocultar a senha (3 segundos neste caso)
+                        passwordVisible = false
+                    }
+                }
+
+                Text(
+                    text = "Senha",
+                    color = Color.White,
+                    fontFamily = Poppins,
+                    fontWeight = FontWeight(300)
                 )
+
+                OutlinedTextField(
+                    value = senha,
+                    onValueChange = { newText ->
+                        senha = newText
+                    },
+                    placeholder = {
+                        Text(
+                            text = "● ● ● ● ●",
+                            fontFamily = Roboto,
+                            color = blueBgAMTranparent,
+                            fontSize = 12.sp
+                        )
+                    },
+                    textStyle = TextStyle(
+                        fontFamily = Roboto,
+                        fontSize = 12.sp,
+                        color = blueBgAM
+                    ),
+                    singleLine = true,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = blueAM,
+                        unfocusedBorderColor = Color.White,
+                        errorBorderColor = Color.Red,
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color.White,
+                        cursorColor = blueBgAM
+                    ),
+                    shape = RoundedCornerShape(8.dp),
+                    visualTransformation = if (passwordVisible) VisualTransformation.None
+                    else PasswordVisualTransformation(),
+                    trailingIcon = {
+                        IconButton(
+                            onClick = {
+                                passwordVisible = !passwordVisible
+                                startHidePasswordTimer()
+                            }
+                        ) {
+                            val iconResId =
+                                if (passwordVisible) R.drawable.visibility_fill0_wght400_grad0_opsz24 else R.drawable.baseline_visibility_off_24
+                            Icon(
+                                painter = painterResource(iconResId),
+                                contentDescription = "ver senha"
+                            )
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+
             }
-            Column(
-                modifier = Modifier.padding(6.dp)
-            ) {
-            }
+
+            Spacer(modifier = Modifier.height(24.dp))
 
             /*COLUNA DO CONFIRMA SENHA*/
             Column(
@@ -212,11 +320,10 @@ fun RegisterScreen() {
 
             ) {
                 Text(
-                    text = "Confirme a senha",
+                    text = "Confirme sua senha",
                     color = Color.White,
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier
-
+                    fontFamily = Poppins,
+                    fontWeight = FontWeight(300)
                 )
 
                 var senha by remember {
@@ -224,13 +331,36 @@ fun RegisterScreen() {
                 }
 
                 OutlinedTextField(
-                    modifier = Modifier
-                        .height(30.dp)
-                        .fillMaxWidth()
-                        .clip(shape = MaterialTheme.shapes.small)
-                        .background(color = Color.White),
                     value = senha,
-                    onValueChange = { senha = it }
+                    onValueChange = { newText ->
+                        senha = newText
+                    },
+                    placeholder = {
+                        Text(
+                            text = "● ● ● ● ●",
+                            fontFamily = Roboto,
+                            color = blueBgAMTranparent,
+                            fontSize = 12.sp
+                        )
+                    },
+                    textStyle = TextStyle(
+                        fontFamily = Roboto,
+                        fontSize = 12.sp,
+                        color = blueBgAM
+                    ),
+                    singleLine = true,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = blueAM,
+                        unfocusedBorderColor = Color.White,
+                        errorBorderColor = Color.Red,
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color.White,
+                        cursorColor = blueBgAM
+                    ),
+                    shape = RoundedCornerShape(8.dp),
+                    visualTransformation = PasswordVisualTransformation(),
+                    modifier = Modifier
+                        .fillMaxWidth()
                 )
             }
 
@@ -261,13 +391,15 @@ fun RegisterScreen() {
             Column(
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Text(text = "Ou cadastre-se com",
+                Text(
+                    text = "Ou cadastre-se com",
                     textAlign = TextAlign.Center,
                     fontSize = 17.sp,
                     color = Color.White,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(12.dp))
+                        .padding(12.dp)
+                )
 
             }
 
@@ -277,20 +409,30 @@ fun RegisterScreen() {
                 horizontalArrangement = Arrangement.Center
             ) {
                 Logos(imageResource = R.drawable.logo_google)
+                Spacer(modifier = Modifier.width(25.dp))
                 Logos(imageResource = R.drawable.logo_facebook)
+                Spacer(modifier = Modifier.width(25.dp))
                 Logos(imageResource = R.drawable.logo_twitter)
             }
             Row(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth().padding(top = 16.dp, bottom = 24.dp),
+                horizontalArrangement = Arrangement.Center
             ) {
                 Text(
-                    text = "Tem uma conta? Logar",
+                    text = "Tem uma conta?  ",
                     color = Color.White,
                     fontSize = 20.sp,
                     textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(vertical = 15.dp)
-                        .fillMaxWidth()
+                )
+
+                Text(
+                    text = "Logar",
+                    color = yellowAM,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.clickable{
+
+                    }
                 )
             }
 
